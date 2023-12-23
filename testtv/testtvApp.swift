5357f1503503fd3eae1e9ecdc8bba2c464af8955
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2023-12-23 22:30:26 +0600

    Initial Commit

---
+++ testtv/testtvApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtvApp.swift
+//  testtv
+//
+//  Created by Ярослав on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct testtvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
